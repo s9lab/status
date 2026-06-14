30b655b2f5ff08692557dcbc4b3c953c0f10d248
Silas Dornbusch <144553383+silas9k@users.noreply.github.com>
2026-06-14 21:42:37 +0200

    t
    t

--- config.js
+++
@@ -1,9 +0,0 @@
-window.S9LAB_STATUS_CONFIG = {
-  projectName: "S9Lab Client",
-  pageTitle: "S9Lab Status",
-  backendUrl: "http://31.70.89.55:25614",
-  apiVersion: "v1",
-  repositoryUrl: "",
-  supportUrl: "",
-  refreshIntervalMs: 60000
-};
